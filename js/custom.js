f9de6c3cdc32c24e40c13a74d14a2a90287f066b
Doug Koellmer <dougkoellmer@hotmail.com>
2022-08-02 22:43:47 -0400

    More updates.

--- js/custom.js
+++ js/custom.js
@@ -42,40 +42,40 @@
 	}
 	// Google Map
 	function gMap () {
-		if ($('.google-map').length) {
-            $('.google-map').each(function () {
-            	// getting options from html 
-            	var mapName = $(this).attr('id');
-            	var mapLat = $(this).data('map-lat');
-            	var mapLng = $(this).data('map-lng');
-            	var iconPath = $(this).data('icon-path');
-            	var mapZoom = $(this).data('map-zoom');
-            	var mapTitle = $(this).data('map-title');
-
-            	// if zoom not defined the zoom value will be 15;
-            	if (!mapZoom) {
-            		var mapZoom = 15;
-            	};
-            	// init map
-            	var map;
-	            map = new GMaps({
-	                div: '#'+mapName,
-	                scrollwheel: false,
-	                lat: mapLat,
-	                lng: mapLng,
-	                zoom: mapZoom
-	            });
-	            // if icon path setted then show marker
-	            if(iconPath) {
-            		map.addMarker({
-		            	icon: iconPath,
-		                lat: mapLat,
-		                lng: mapLng,
-		                title: mapTitle
-		            });
-            	}
-            });  
-		};
+		// if ($('.google-map').length) {
+//             $('.google-map').each(function () {
+//             	// getting options from html
+//             	var mapName = $(this).attr('id');
+//             	var mapLat = $(this).data('map-lat');
+//             	var mapLng = $(this).data('map-lng');
+//             	var iconPath = $(this).data('icon-path');
+//             	var mapZoom = $(this).data('map-zoom');
+//             	var mapTitle = $(this).data('map-title');
+//
+//             	// if zoom not defined the zoom value will be 15;
+//             	if (!mapZoom) {
+//             		var mapZoom = 15;
+//             	};
+//             	// init map
+//             	var map;
+// 	            map = new GMaps({
+// 	                div: '#'+mapName,
+// 	                scrollwheel: false,
+// 	                lat: mapLat,
+// 	                lng: mapLng,
+// 	                zoom: mapZoom
+// 	            });
+// 	            // if icon path setted then show marker
+// 	            if(iconPath) {
+//             		map.addMarker({
+// 		            	icon: iconPath,
+// 		                lat: mapLat,
+// 		                lng: mapLng,
+// 		                title: mapTitle
+// 		            });
+//             	}
+//             });
+// 		};
 	}
 	// revolution slider
 	function revolutionSliderActiver () {
